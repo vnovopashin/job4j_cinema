a1c5d69e32abce0e1696cd7a3c8c310aa14f9ca4
vnovopashin <novopashinva@gmail.com>
2023-02-19 22:08:18 +0700

    2. Сервис - Кинотеатр [#504869].  Настроил проект, создал БД cinema и схему.

---
+++ src/main/java/ru/job4j/cinema/Job4jCinemaApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/job4j/cinema/Job4jCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
